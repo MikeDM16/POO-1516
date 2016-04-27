73df4aab70084918fe3e8471d82be373ed6cbfc9
Rui Filipe Castro Leite <ruicastro@outlook.com>
2016-04-27 17:58:43 +0100

    hello

--- ImOObiliária/Comprador.java
+++ ImOObiliária/Comprador.java
@@ -1,88 +1,34 @@
-
-/**
- * Escreva a descrição da classe Comprador aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
-
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Comprador {
-    
+public class Comprador extends Atores {
     // variáveis de instância
-    private String nome, email, password, morada, dataN;
     private ArrayList<Imoveis> favoritos;
     
     /**
      * Construtor para objetos da classe Comprador
      */
-    public Comprador() {
-        this(null, null, null, null, null); 
-    }
     public Comprador(String n, String e, String p, String m, String d) {
-        this.nome = n;
-        this.email = e;
-        this.password = p;
-        this.morada = m;
-        this.dataN = d;
+        super(n,e,p,m,d);
         this.favoritos = new ArrayList<>();
     }
-    public Comprador(Comprador c){
-        this.nome = c.getNome();
-        this.email = c.getEmail();
-        this.password = c.getPass(); 
-        this.morada = c.getMorada();
-        this.dataN = c.getDataN();
-        this.favoritos = copiaArrayL(c.getFav());
+    public Comprador(Atores a) {
+        super(a);
+        this.favoritos = new ArrayList<>();
     }
     
     /**
      * Métodos de instância da classe Comprador
      */
-       
-    public String getNome() {
-        return this.nome;
-    }
-    public String getEmail() {
-        return this.email;
-    }
-    public String getPass() {
-        return this.password;
-    }
-    public String getMorada() {
-        return this.morada;
-    }
-    public String getDataN() {
-        return this.dataN;
-    }
     public ArrayList<Imoveis> getFav() {
         return this.favoritos;
     }
-    
-    public void setNome(String n) {
-        this.nome = n;
-    }
-    public void setEmail(String e) {
-        this.email = e;
-    }
-    public void setPass(String p) {
-        this.password = p;
-    }
-    public void setMorada(String m) {
-        this.morada = m;
-    }
-    public void setDataN(String d) {
-        this.dataN = d;
-    }
-    
     public Comprador clone(Comprador c) {
-        Comprador novo = new Comprador(c.getNome(), c.getEmail(), c.getPass(), c.getMorada(), c.getDataN());
+        Atores novoAtor = new Atores(c.getNome(), c.getEmail(), c.getPass(), c.getMorada(), c.getDataN());
+        Comprador novo = new Comprador(novoAtor);
         novo.favoritos = copiaArrayL(c.getFav());
-        return novo;        
-        /*return (new Comprador(c.getNome().clone(), c.getEmail().clone(), c.getPass().clone(), c.getMorada().clone(), c.getDataN().clone()));*/
+        return novo;
     }
 
     /**
--- ImOObiliária/Vendedor.java
+++ ImOObiliária/Vendedor.java
@@ -1,67 +1,29 @@
-
-/**
- * Escreva a descrição da classe Vendedor aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
-
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Vendedor {
+public class Vendedor extends Atores {
     
     // variáveis de instância
-    private String nome, email, password, morada, dataN;
     private ArrayList<Imoveis> portfolio, historico;
     
     /**
      * Construtor para objetos da classe Vendedor
      */
-    public Vendedor() {
-        this(null, null, null, null, null);
-    }
     public Vendedor(String n, String e, String p, String m, String d) {
-        ArrayList<Imoveis> i = new ArrayList<>();
-        ArrayList<Imoveis> h = new ArrayList<>();
-        this.nome = n;
-        this.email = e;
-        this.password = p;
-        this.morada = m;
-        this.dataN = d;
-        this.portfolio = i;
-        this.historico = h;
+        super(n,e,p,m,d);
+        this.portfolio = new ArrayList<>();
+        this.historico = new ArrayList<>();
     }
-    public Vendedor(Vendedor c){
-        this.nome = c.getNome();
-        this.email = c.getEmail();
-        this.password = c.getPass(); 
-        this.morada = c.getMorada();
-        this.dataN = c.getDataN();
-        this.portfolio = copiaArrayL(c.getPortf());
-        this.historico = copiaArrayL(c.getHist());
+    public Vendedor(Atores c){
+        super(c);
+        this.portfolio = new ArrayList<>();
+        this.historico = new ArrayList<>();
     }
     
     /**
      * Métodos de instância da classe Vendedor
      */
-       
-    public String getNome() {
-        return this.nome;
-    }
-    public String getEmail() {
-        return this.email;
-    }
-    public String getPass() {
-        return this.password;
-    }
-    public String getMorada() {
-        return this.morada;
-    }
-    public String getDataN() {
-        return this.dataN;
-    }
     public ArrayList<Imoveis> getPortf() {
         return (this.portfolio);
     }
@@ -69,28 +31,12 @@ public class Vendedor {
         return (this.historico);
     }
     
-    public void setNome(String n) {
-        this.nome = n;
-    }
-    public void setEmail(String e) {
-        this.email = e;
-    }
-    public void setPass(String p) {
-        this.password = p;
-    }
-    public void setMorada(String m) {
-        this.morada = m;
-    }
-    public void setDataN(String d) {
-        this.dataN = d;
-    }
-    
     public Vendedor clone(Vendedor c) {
-        Vendedor novo = new Vendedor(c.getNome(), c.getEmail(), c.getPass(), c.getMorada(), c.getDataN());
+        Atores novoAtor = new Atores(c.getNome(), c.getEmail(), c.getPass(), c.getMorada(), c.getDataN());
+        Vendedor novo = new Vendedor(novoAtor);
         novo.portfolio = copiaArrayL(c.getPortf());
         novo.historico = copiaArrayL(c.getHist());
         return novo;
-        /*return (new Vendedor(c.getNome().clone(), c.getEmail().clone(), c.getPass().clone(), c.getMorada().clone(), c.getDataN().clone()));*/
     }
 
     /**
